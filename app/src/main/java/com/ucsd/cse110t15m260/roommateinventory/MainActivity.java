6fb0477d9f9df4549cc3f31643a5414da13df3d4
Siddharth Atre <sidatre@gmail.com>
2016-01-23 14:05:38 -0800

    Integrated Parse framework.

--- app/src/main/java/com/ucsd/cse110t15m260/roommateinventory/MainActivity.java
+++ app/src/main/java/com/ucsd/cse110t15m260/roommateinventory/MainActivity.java
@@ -5,15 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.*;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
