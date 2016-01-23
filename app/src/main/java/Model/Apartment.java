8204eab0dc47b7b107e7f99606dd6cc0ece09d63
Siddharth Atre <sidatre@gmail.com>
2016-01-23 14:41:04 -0800

    Created Apartment Class

--- app/src/main/java/Model/Apartment.java
+++ app/src/main/java/Model/Apartment.java
@@ -1,7 +1,48 @@
 package Model;
 
+import com.parse.*;
+
 /**
  * Created by satre on 1/23/16.
  */
-public class Apartment {
+@ParseClassName(Apartment.className)
+public class Apartment extends ParseObject {
+    public final static String className = "Apartment";
+
+    /***********************
+     * Properties
+     */
+
+    /**
+     * Default Constructor
+     */
+    public Apartment() {}
+
+    /**
+     * Fetches the name of this apartment
+     * @return String
+     */
+    public String getName() {
+        return getString("name");
+    }
+
+    /**
+     * Updates the name of this apartment to the given value.
+     * @param String newName, the name to set.
+     */
+    public void setName(String newName){
+        put("name", newName);
+    }
+
+    /**
+     * Get the relation to User class that contains members of this apartment.
+     * @return ParseRelation
+     */
+   public ParseRelation<ParseObject> getUserRelation() {
+       return getRelation("users");
+   }
+
+    /**
+     * 
+     */
 }
--- app/src/main/java/com/ucsd/cse110t15m260/roommateinventory/MainActivity.java
+++ app/src/main/java/com/ucsd/cse110t15m260/roommateinventory/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ucsd.cse110t15m260.roommateinventory;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,8 +11,21 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.parse.*;
+
+import Model.Apartment;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String APPLICATION_ID = "uymjS3lXDmOqNv0IPYhLS2HFhkzoVhLaCyVAyM6o";
+    private static final String CLIENT_KEY = "2CTlHXNSyVRYSRJ0l9OUuXNgQUX5MTwZyNvYCgzX";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -51,5 +68,62 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //--------------------------------------------
+
+    /**
+     * Parse Setup function, place all setup code here.
+     *
+     * @return
+     */
+    private void setupParse() {
+        //Register class of model
+        ParseObject.registerSubclass(Apartment.class);
+
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, MainActivity.APPLICATION_ID, MainActivity.CLIENT_KEY);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.ucsd.cse110t15m260.roommateinventory/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.ucsd.cse110t15m260.roommateinventory/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
