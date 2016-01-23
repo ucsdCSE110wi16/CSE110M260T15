626defb201298d8218309a3f8732dd1be4cac205
Siddharth Atre <sidatre@gmail.com>
2016-01-23 15:04:51 -0800

    Added getters and setters to Apartment Class

--- app/src/main/java/Model/Apartment.java
+++ app/src/main/java/Model/Apartment.java
@@ -9,14 +9,18 @@ import com.parse.*;
 public class Apartment extends ParseObject {
     public final static String className = "Apartment";
 
+    /**
+     * Default Constructor
+     */
+    public Apartment() {
+        super();
+
+    }
+
     /***********************
      * Properties
      */
 
-    /**
-     * Default Constructor
-     */
-    public Apartment() {}
 
     /**
      * Fetches the name of this apartment
@@ -28,7 +32,7 @@ public class Apartment extends ParseObject {
 
     /**
      * Updates the name of this apartment to the given value.
-     * @param String newName, the name to set.
+     * @param newName, the name to set.
      */
     public void setName(String newName){
         put("name", newName);
@@ -38,11 +42,64 @@ public class Apartment extends ParseObject {
      * Get the relation to User class that contains members of this apartment.
      * @return ParseRelation
      */
-   public ParseRelation<ParseObject> getUserRelation() {
-       return getRelation("users");
+   public ParseRelation<Person> getUserRelation() {
+
+       ParseRelation<Person> roomies = getRelation("users");
+       return roomies;
    }
 
     /**
-     * 
+     * Adds the given person the relation that contains the members of this apartment.
+     * @param person
      */
+    public void addPersonToApartment( Person person) {
+        if (person == null) {
+            return;
+        }
+
+        ParseRelation<Person> relation = getUserRelation();
+        relation.add(person);
+        incrementNumberOfResidents();
+        saveInBackground();
+    }
+
+    public void removePersonFromApartment( Person person) {
+        if (person == null) {
+            return;
+        }
+
+        ParseRelation<Person> relation = getUserRelation();
+        relation.remove(person);
+        decrementNumberOfResidents();
+        saveInBackground();
+    }
+
+    /**
+     * Returns the number of people the live in this apartment
+     * @return the occupancy of this apartment.
+     */
+    public int getNumberOfResidents() {
+        return getInt("numberOfResidents");
+    }
+
+    /**
+     * Increments the number of people living here and returns the new value.
+     * @return the new occupancy.
+     */
+    public int incrementNumberOfResidents() {
+        increment("numberOfResidents");
+        return getNumberOfResidents();
+    }
+
+    /**
+     * Decrements the number of people living here and returns the updated value.
+     * @return the new occupancy
+     */
+    public int decrementNumberOfResidents() {
+        if( getNumberOfResidents() > 0) {
+            increment("numberOfResidents", -1);
+        }
+
+        return getNumberOfResidents();
+    }
 }
