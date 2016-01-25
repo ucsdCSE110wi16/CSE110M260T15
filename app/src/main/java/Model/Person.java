5007f24549a1be73acde18a0378c40a1274ec625
Siddharth Atre <sidatre@gmail.com>
2016-01-25 09:38:10 -0800

    Added documentation and changed a couple methods to be static

--- app/src/main/java/Model/Person.java
+++ app/src/main/java/Model/Person.java
@@ -11,14 +11,22 @@ public class Person extends ParseUser
 {
     public Person()
     {
-
+        super();
     }
 
+    /**
+     * Accesor to the residence of this person
+     * @return The apartment which they live in.
+     */
     public ParseObject getApartment()
     {
         return ParseUser.getCurrentUser().getParseObject("apartment");
     }
 
+    /**
+     * Registers this person as living in the given apartment, iff they do not already live elsewhere
+     * @param apartment The apartment to add them to.
+     */
     public void setApartment(Apartment apartment)
     {
         if(ParseUser.getCurrentUser().getParseObject("apartment") == null)
@@ -27,16 +35,28 @@ public class Person extends ParseUser
         }
     }
 
+    /**
+     * Get this person's first name
+     * @return Their first name
+     */
     public String getFirstName () {
         return ParseUser.getCurrentUser().getString("firstName");
     }
 
+    /**
+     * Get this person's last name
+     * @return Their last name
+     */
     public String getLastName() {
         return ParseUser.getCurrentUser().getString("lastName");
     }
 
-
-    public void createUser(String userName, String password)
+    /**
+     * Convenience method to sign up a new user.
+     * @param userName
+     * @param password
+     */
+    public static void createUser(String userName, String password)
     {
         ParseUser.getCurrentUser().logOut();
         ParseUser newUser = new ParseUser();
@@ -45,7 +65,12 @@ public class Person extends ParseUser
         newUser.signUpInBackground();
     }
 
-    public void loginUser(String username, String password)
+    /**
+     * Convenience method to login a perosn with the given username and password.
+     * @param username
+     * @param password
+     */
+    public static void loginUser(String username, String password)
     {
         ParseUser.logInInBackground(username,password);
     }
