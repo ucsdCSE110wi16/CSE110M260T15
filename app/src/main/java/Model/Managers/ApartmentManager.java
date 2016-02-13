6dbcd39d48bae3ebbbbaf603098999773a6c8829
Siddharth Atre <sidatre@gmail.com>
2016-02-13 15:48:01 -0800

    Created more helper methods

--- app/src/main/java/Model/Managers/ApartmentManager.java
+++ app/src/main/java/Model/Managers/ApartmentManager.java
@@ -5,6 +5,7 @@ package Model.Managers;
  */
 
 import Model.Apartment;
+import Model.Person;
 
 /**
  * This class defines logic associated with the Apartment class. It also holds the single instance of the current Apartment object in memory.
@@ -17,7 +18,7 @@ public class ApartmentManager {
     private ApartmentManager(){}
 
     /**
-     * The apartment that the user lives in. Note that it can be null if there is logged in user, or if he/she has not been added to an apartment.
+     * The apartment that the user lives in. Note that it can be null if there is no logged in user, or if he/she has not been added to an apartment.
      * WARNING: This is the ONLY place that the current apartment should be stored. No other class should have an instance var that holds a copy.
      */
     private Apartment currentApartment;
@@ -25,6 +26,32 @@ public class ApartmentManager {
 
     public final Apartment getCurrentApartment() {
         return currentApartment;
+    }
+
+    /**
+     * Adds the given person to the apartment
+     * @param newMate The person who is moving in.
+     * @return True if the addition succeeds.
+     */
+    public boolean addPersonToCurrentApartment(Person newMate)  {
+        if (currentApartment == null ) {
+            return false;
+        }
+
+        return currentApartment.addPersonToApartment(newMate);
+    }
+
+    /**
+     * Removes the given person from the apartment.
+     * @param formerMate The person who is moving out.
+     * @return True if the removal succeeds.
+     */
+    public boolean removePersonFromCurrentApartment( Person formerMate) {
+        if (currentApartment == null) {
+            return false;
+        }
+
+        return currentApartment.removePersonFromApartment(formerMate);
     }
 
 }
