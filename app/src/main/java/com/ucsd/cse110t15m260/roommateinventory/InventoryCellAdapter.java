8792b7ba3a12dfdae2b1c25bf7bb763fcf1bf5e8
Kevin Hwang <kevinhwang@eng.ucsd.edu>
2016-02-21 17:02:57 -0800

    Fix compilation error with InventoryCellAdapater onClickListener; enforce quantity constrats in InventoryItem model

--- app/src/main/java/com/ucsd/cse110t15m260/roommateinventory/InventoryCellAdapter.java
+++ app/src/main/java/com/ucsd/cse110t15m260/roommateinventory/InventoryCellAdapter.java
@@ -60,7 +60,7 @@ public class InventoryCellAdapter<T> extends ArrayAdapter<InventoryItem> {
             @Override
             public void onClick(View v) {
 
-                getItem(pos).setQuantity((int) getItem(pos).getQuantity() + 1);
+                getItem(pos).increment();
                 getItem(pos).saveInBackground();
                 notifyDataSetChanged();
 
@@ -69,9 +69,11 @@ public class InventoryCellAdapter<T> extends ArrayAdapter<InventoryItem> {
         decButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(getItem(pos).getQuantity()  == 0)
+                if(getItem(pos).getQuantity().intValue() == 0) {
                     return;
-                getItem(pos).setQuantity((int) getItem(pos).getQuantity() - 1);
+                }
+
+                getItem(pos).decrement();
                 getItem(pos).saveInBackground();
                 notifyDataSetChanged();
             }
