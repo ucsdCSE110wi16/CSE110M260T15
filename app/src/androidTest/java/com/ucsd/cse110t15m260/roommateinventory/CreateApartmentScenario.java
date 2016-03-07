824ac54ceb8f9ec56287d8bc6c6cb37dc104f306
lww001@ucsd.edu <lww001@ucsd.edu>
2016-03-07 13:48:22 -0800

    wrote scenario test for creating a new user who creates a new apartment

--- app/src/androidTest/java/com/ucsd/cse110t15m260/roommateinventory/CreateApartmentScenario.java
+++ app/src/androidTest/java/com/ucsd/cse110t15m260/roommateinventory/CreateApartmentScenario.java
@@ -4,8 +4,8 @@ import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-
-import com.parse.ParseUser;
+import android.util.Log;
+import android.widget.TextView;
 
 import org.junit.After;
 import org.junit.Before;
@@ -20,16 +20,16 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static com.parse.ParseUser.getCurrentUser;
-//import android.support.test.espresso.*;
 
-/* Created by jlsmith on 2/20/16.
-*/
+/**
+ * Created by Leo Wong on 3/7/2016.
+ */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class CreateUserTest {
+public class CreateApartmentScenario {
 
     @Rule
     public ActivityTestRule<RegisterActivity> mActivityRule = new ActivityTestRule<>(RegisterActivity.class);
@@ -52,8 +52,8 @@ public class CreateUserTest {
     public void setUp() throws Exception {
         super.setUp();
         getActivity();
-    }
-    */
+    }*/
+
     @Before
     public void initValidString() {
         mName = "newUser";
@@ -70,7 +70,6 @@ public class CreateUserTest {
     @Test
     public void createNewUser() throws InterruptedException {
 
-
         // Type text and then press the button
         onView(withId(R.id.name)).perform(typeText(mName), closeSoftKeyboard());
         onView(withId(R.id.email)).perform(typeText(mEmail), closeSoftKeyboard());
@@ -81,35 +80,14 @@ public class CreateUserTest {
         onView(withId(R.id.password)).check(matches(withText("newUser")));
         onView(withId(R.id.email_register_button)).perform(click());
 
-        //mActivityRule.wait((long) 5000);
-        //MainActivity mainActivity = (MainActivity) mActivityRule.getActivity().getBaseContext();
-
-        //MaimDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        //mainActivity.mDrawerLayout.openDrawer(GravityCompat.START);
-
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
 
-        //onData(anything()).inAdapterView(withId(R.id.list_view)).atPosition(0).perform(click());
-        //openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
-        //onView(withText("My Apartment")).perform(click());
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.my_apt));
 
         Thread.sleep(1000);
 
-        /*
-        mAptName = "apt";
-        mStreet1 = "street1";
-        mStreet2 = "street2";
-        mCity = "city";
-        mState = "state";
-        mZipCode = "123";
-        */
-
         //Enter the create apartment activity
         onView(withId(R.id.create_apt)).perform(click());
-
-        Thread.sleep(1000);
 
         //Typing in all required fields
         onView(withId(R.id.apartment_name)).perform(typeText(mAptName), closeSoftKeyboard());
@@ -119,6 +97,7 @@ public class CreateUserTest {
         onView(withId(R.id.state)).perform(typeText(mState), closeSoftKeyboard());
         onView(withId(R.id.zip_code)).perform(typeText(mZipCode), closeSoftKeyboard());
 
+        //Check that all textviews are filled
         onView(withId(R.id.apartment_name)).check(matches(withText(mAptName)));
         onView(withId(R.id.street_1)).check(matches(withText(mStreet1)));
         onView(withId(R.id.street_2)).check(matches(withText(mStreet2)));
@@ -126,9 +105,30 @@ public class CreateUserTest {
         onView(withId(R.id.state)).check(matches(withText(mState)));
         onView(withId(R.id.zip_code)).check(matches(withText(mZipCode)));
 
+        Thread.sleep(1000);
+
+        //Press create apaartment to create the apartment
         onView(withId(R.id.create_apt_btn)).perform(click());
 
+        Thread.sleep(1000);
 
+        //Check that an actual apartment was made
+        Person person = Person.getCurrentPerson();
+        String aptID = person.getApartment().getObjectId();
+        onView(withId(R.id.apartment_invitation_id)).check(matches(withText(aptID)));
+
+        Thread.sleep(1000);
+
+        //Press the actionbar back button
+        onView(withContentDescription("Navigate up")).perform(click());
+
+        //Now back in the main activity main fragment
+        String mainStr = "Welcome, " + person.getString("name") + "!\n" +
+                "Your User ID is: " + person.getObjectId() + "\n" +
+                "Your Apartment is: " + person.getApartment().getObjectId();
+
+        //Check that the main fragment string matches what it's supposed to be
+        onView(withId(R.id.textview_welcome)).check(matches(withText(mainStr)));
 
         Thread.sleep(1000);
     }
@@ -137,7 +137,6 @@ public class CreateUserTest {
     public void deleteUser() {
 
     }
-
 
     @After
     public void tearDown() throws Exception {
